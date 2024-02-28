ff627608daa97b446793259b6e8145c2cb7515f1
Jordan <jordancdiaz04@gmail.com>
2024-02-27 21:57:36 -0500

    save progress

--- server/app.js
+++ server/app.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const pool = require("./db");
-const port = 3000;
+const port = 8080;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  await pool.query('CREATE TABLE IF NOT EXISTS users ( id SERIAL PRIMARY KEY, name VARCHAR(100), address VARCHAR(100)) ')
+app.get("/setup", async (req, res) => {
+  await pool.query(
+    "CREATE TABLE IF NOT EXISTS users ( id SERIAL PRIMARY KEY, name VARCHAR(100), address VARCHAR(100)) "
+  );
   res.send("Hello Jordan");
 });
 
 app.get("/users", async (req, res) => {
-  const data = await pool.query('SELECT * FROM users');
+  const data = await pool.query("SELECT * FROM users");
   res.status(200).send(data.rows);
 });
 
@@ -21,7 +23,7 @@ app.post("/users", async (req, res) => {
   try {
     const { name, address } = req.body;
     const response = await pool.query(
-      'INSERT INTO users (name, address) VALUES ($1, $2)',
+      "INSERT INTO users (name, address) VALUES ($1, $2)",
       [name, address]
     );
     res.status(200).send({
@@ -30,24 +32,12 @@ app.post("/users", async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).send({
-      message: "get fucked buddy",
-    });
-  }
-});
-
-app.get("/setup", async (req, res) => {
-  try {
-    await pool.query(
-      'CREATE TABLE users( id SERIAL PRIMARY KEY, name VARCHAR(100), address VARCHAR(100))'
-    );
-  } catch (err) {
-    console.log(err);
-    res.status(500).send({
-      message: "go fuck yourself",
+      message: "get error buddy",
     });
   }
 });
 
 //http://localhost:1337
+
 app.listen(port, "0.0.0.0");
 console.log(`Running on http://0.0.0.0:${port}`);
